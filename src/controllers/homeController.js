09b93b8b42202d1ff22984d2a3a24452b0e95d59
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-13 22:24:56 +0100

    Move the routes into the controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,4 +2,8 @@ import { Router } from "express";
 
 const homeController = Router();
 
+homeController.get('/', (req, res) => {
+    res.send('It Works | Home Controller');
+});
+
 export default homeController;
--- src/index.js
+++ src/index.js
@@ -8,8 +8,4 @@ app.use(express.static('src/public'));
 // Body parser middleware
 app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-    res.send('It works');
-});
-
 app.listen(3000, console.log('Server is listening on http://localhost:3000...'));
