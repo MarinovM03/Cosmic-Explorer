b8698b819742a4711466929265514b6059414e0b
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-13 23:37:28 +0100

    Add layout and display home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const homeController = Router();
 
 homeController.get('/', (req, res) => {
-    res.send('It Works | Home Controller');
+    res.render('home');
 });
 
 export default homeController;
