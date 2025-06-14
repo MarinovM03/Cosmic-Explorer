72fc0d352b3155e5252f8fbc48d3c92532fa902b
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-14 16:13:25 +0100

    Add rePassword validation

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,10 @@ import User from '../models/User.js';
 
 export default {
     register(userData) {
+        if (userData.password === userData.rePassword) {
+            throw new Error('Password Missmatch!');
+        }
+
         return User.create(userData);
     }
 }
