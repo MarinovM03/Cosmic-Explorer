a85c95ea62543128dfb1c1ab1832f81d7add33d5
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-14 15:28:43 +0100

    Config userController

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import homeController from "./controllers/homeController.js";
+import userController from "./controllers/userController.js";
 
 const routes = Router();
 
 routes.use(homeController);
+routes.use('/user', userController);
 
 export default routes;
