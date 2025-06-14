81f03e75aaf3fdc8d5535bc6d30fa425c001dd1b
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-14 18:16:22 +0100

    Fix user exists validation

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,13 +1,13 @@
 import User from '../models/User.js';
 
 export default {
-    register(userData) {
-        if (userData.password === userData.rePassword) {
+    async register(userData) {
+        if (userData.password !== userData.rePassword) {
             throw new Error('Password Missmatch!');
         }
 
-        const user = User.findOne({ email: userData.email });
-        
+        const user = await User.findOne({ email: userData.email });
+
         if (user) {
             throw new Error('User already exists!');
         }
