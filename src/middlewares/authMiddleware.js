f9dfdc222acdad2db4570b838285fb1c297c4abe
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-14 23:14:24 +0100

    Verify token, handle invalid token, and attach user

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,24 @@
-import { AUTH_COOKIE_NAME } from "../config/index.js";
+import jsonwebtoken from 'jsonwebtoken';
+
+import { AUTH_COOKIE_NAME, JWT_SECRET } from "../config/index.js";
 
 export function auth(req, res, next) {
     const token = req.cookies[AUTH_COOKIE_NAME];
 
-    next();
+    if (!token) {
+       return next();
+    }
+
+    try {
+        const user = jsonwebtoken.verify(token, JWT_SECRET);
+        
+        req.user = user;
+        res.locals.user = user;
+        res.locals.isAuthenticated = true;
+
+    } catch (err) {
+        res.clearCookie(AUTH_COOKIE_NAME);
+
+        res.redirect('/users/login');
+    }
 }
