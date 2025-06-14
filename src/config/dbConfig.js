63fc303cd1df12628cbeeb5a065d803d95baaa4c
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-14 13:58:05 +0100

    Add DB error handling

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,8 +4,14 @@ async function initDatabase() {
     const dbUrl = `mongodb://localhost:27017`;
     const dbName = 'cosmic-explorer';
 
-    await mongoose.connect(dbUrl, { dbName });
- 
+    try {
+        await mongoose.connect(dbUrl, { dbName });
+        console.log('DB Connected Successfully');
+        
+    } catch (err) {
+        console.log('DB Connection Failed');
+        console.log(err.message);
+    }
 }
 
 export default initDatabase;
