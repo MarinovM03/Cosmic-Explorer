34027c06ae075eb78bc53b2b8656596a1e127f75
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-15 18:45:10 +0100

    Update jsonwebtoken

--- src/lib/jsonwebtoken.js
+++ src/lib/jsonwebtoken.js
@@ -2,9 +2,11 @@ import util from 'util';
 import baseJWT from 'jsonwebtoken';
 
 const sign = util.promisify(baseJWT.sign);
+const decode = util.promisify(baseJWT.decode);
 const verify = util.promisify(baseJWT.verify);
 
 export default {
     sign,
+    decode,
     verify,
 };
